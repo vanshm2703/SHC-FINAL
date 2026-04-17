d20f95b90ed0319a9b9a4e8100c6a034222a7d09
Vansh Mehta <vanshm2703@gmail.com>
2026-04-17 19:27:27 +0530

    minor change

--- Self-Healing-Code-1/src/components/component/exampletab.tsx
+++ Self-Healing-Code-1/src/components/component/exampletab.tsx
@@ -10,7 +10,7 @@ export default function TabsDemo() {
         content: (
           <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-blue-900 to-blue-950">
             <p>Beautiful design meets powerful functionality</p>
-            <DummyContent imageSrc="/n1.jpeg" />
+            <DummyContent imageSrc="/f1.jpeg" />
           </div>
         ),
       },
